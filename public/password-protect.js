557c8a7b7a813081c5a8b4cbc149a618efae6341
Emma Bjørg <emso0005@stud.kea.dk>
2024-10-28 11:01:33 +0100

    password slået fra

--- public/password-protect.js
+++
@@ -1,6 +0,0 @@
-const password = prompt("Enter the password to access this site:");
-
-if (password !== "intothewoods") {
-    alert("Incorrect password!");
-    window.location.href = "https://netlify.com";
-}
