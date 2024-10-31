2425b87c3fb6979b21ec1de971acd4666b211396
Emma Bjørg <emso0005@stud.kea.dk>
2024-10-31 22:25:01 +0100

    inviewproblems

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import inView from 'in-view';
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
+  integrations: [tailwind(), alpinejs(), inView()],
 });
