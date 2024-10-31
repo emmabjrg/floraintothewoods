7c898559c800883ed8faef7d39fcf9f090c62ec9
Emma Bjørg <emso0005@stud.kea.dk>
2024-10-31 23:53:09 +0100

    inview

--- src/assets/images/fade-in.js
+++
@@ -1,8 +0,0 @@
-import inView from 'in-view';
-
-document.addEventListener("DOMContentLoaded", function () {
-    inView('.fade-in').on('enter', el => {
-        console.log("Element entered viewport:", el); 
-        el.classList.add('in-view');
-    });
-});
