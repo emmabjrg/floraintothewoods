c76144af0f7c67216ebc6ea32c3a3746b5e8131e
Emma Bjørg <emso0005@stud.kea.dk>
2024-10-31 22:31:10 +0100

    astro add igen

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import inView from 'in-view';
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), inView()],
+  integrations: [tailwind(), alpinejs()],
 });
